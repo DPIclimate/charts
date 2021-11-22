5d202c2c66797e8d1a8865ef441c8d6a7c262ac1
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2021-11-22 14:53:23 +1100

    Changed some widgets to use the Ubidots() device to get the API token.

---
+++ templates/html/ubidots.js
@@ -0,0 +1,36 @@
+/*
+var ubidots = new Ubidots();
+
+// Token is BBAU-...
+ubidots.on('receivedToken', function (data) {
+});
+
+// Will be called on static dashboards, data is null in that case.
+ubidots.on('selectedDevice', function (data) {
+});
+
+data is an object {"start": ts, "end": ts} where ts are epoch timestamps.
+ubidots.on('selectedDashboardDateRange', function (data) {
+});
+*/
+
+class Ubidots {
+    constructor() {
+    }
+
+    on(type, callback) {
+        if (type == 'receivedToken') {
+            console.log("Dummy Ubidots object token.")
+            callback('...'); // Put an Ubidots token here.
+        }
+
+        // TODO: Consider programatically adding a div to the top of the page with
+        // a date picker that can update this object's start and end timestamps.
+        if (type == 'selectedDashboardDateRange') {
+          console.log("Dummy Ubidots object date range.")
+          var start = Date.now();
+          var end = start - 7 * 24 * 60 * 60 * 1000;
+          callback({"start": start, "end": end});
+        }
+    }
+}
