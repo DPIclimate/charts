ec29497ec624bab05d030475aa42af61b34891c7
David Taylor <david.a.taylor@dpi.nsw.gov.au>
2022-05-23 11:02:12 +1000

    Changed wind-barb widget to avoid most redraws, added 'ready' event to dummy Ubidots object.

--- templates/html/ubidots.js
+++ templates/html/ubidots.js
@@ -15,25 +15,32 @@ ubidots.on('selectedDashboardDateRange', function (data) {
 */
 
 class Ubidots {
-    constructor() {
-		var end = Date.now();
-		var start = end - 7 * 24 * 60 * 60 * 1000;
-		var dashboardDateRange = {"start": start, "end": end}
+  token = "BBAU-...";
+  dashboardDateRange;
+
+  constructor() {
+    var end = Date.now();
+    var start = end - 7 * 24 * 60 * 60 * 1000;
+    this.dashboardDateRange = { "start": start, "end": end }
+  }
+
+  on(type, callback) {
+    if (type == 'receivedToken') {
+      console.log("Dummy Ubidots object token.")
+      callback(this.token); // Put an Ubidots token here.
     }
 
-    on(type, callback) {
-        if (type == 'receivedToken') {
-            console.log("Dummy Ubidots object token.")
-            callback('...'); // Put an Ubidots token here.
-        }
-
-        // TODO: Consider programatically adding a div to the top of the page with
-        // a date picker that can update this object's start and end timestamps.
-        if (type == 'selectedDashboardDateRange') {
-          console.log("Dummy Ubidots object date range.")
-          var end = Date.now();
-          var start = end - 7 * 24 * 60 * 60 * 1000;
-          callback({"start": start, "end": end});
-        }
+    // TODO: Consider programatically adding a div to the top of the page with
+    // a date picker that can update this object's start and end timestamps.
+    if (type == 'selectedDashboardDateRange') {
+      console.log("Dummy Ubidots object date range.")
+      callback(this.dashboardDateRange);
     }
+
+    if (type == 'ready') {
+      console.log("Dummy Ubidots ready.")
+      // The callback function can get token and dashboardDateRange from the Ubidots object properties.
+      callback();
+    }
+  }
 }
